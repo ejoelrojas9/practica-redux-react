0677b2b3a2fab170e0eb50b29228d3fcce228cba
Joel Rojas <ejoelrojas9@gmail.com>
2022-03-29 14:51:31 -0500

    Configuración para conectar el estado con los props, importación de la propiedad Connect de React-Redux

--- src/comṕonents/CantidadPokemon.jsx
+++ src/comṕonents/CantidadPokemon.jsx
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux'; //conectamos los componentes
 
 class CantidadPokemon extends Component {
   render(){
     return(
       <React.Fragment>
-        Unidades: 30
-      </React.Fragment>
+        Unidades: {this.props.game_shop.pokemon}  
+      </React.Fragment> //{this.props.game_shop.pokemon}  //acceso a los this.props
     )
   }
 }
 
-export default CantidadPokemon;
+const mapStateToProps = (state) => {  //Constante que convierte el estado en props  (Accedemos al estado y devolvemos un objeto)
+  return {
+    game_shop: state.game_shop
+  }
+}
+
+export default connect(mapStateToProps)(CantidadPokemon); //doble función (función currificada, funcion que evuelve otra función)
