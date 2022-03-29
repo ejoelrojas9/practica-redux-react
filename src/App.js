6c647c15b3236ff325559a43cc24d1720d7ba321
Joel Rojas <ejoelrojas9@gmail.com>
2022-03-29 13:12:17 -0500

    Enlace de la aplicación con react por medio de la propiedad Provider de la libreria React-Redux para envolver la app y mostrala en el browser

--- src/App.js
+++ src/App.js
@@ -4,36 +4,36 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import CantidadPokemon from './comṕonents/CantidadPokemon';
 import ComprarPokemon from './comṕonents/ComprarPokemon';
+import { Provider } from 'react-redux';
 import store from './redux/store';
 
 function App() {
-    console.log(store.getState());
     return ( 
-    <div className = "App container">
-        <div className='row'>
-            <div className='col-12'>
+    <Provider store={store}>
+        <div className = "App container">
+            <div className='row'>
+                <div className='col-12'>
 
-                <div className='card mt-5' style={{maxWidth: '370px'}}>
-                    <div className='row no-gutters'>
-                        <div className='col-4'>
-                            <img src={require('./img/pokemon.jpg')} alt='pokemon' className='card-img' />
-
-                        </div>
-                        <div className="col-8">
-                            <div className="card-body">
-                                <div className='card-title h3 text-center'>
-                                    <CantidadPokemon />
-                                </div>
-                                <ComprarPokemon />
+                    <div className='card mt-5' style={{maxWidth: '370px'}}>
+                        <div className='row no-gutters'>
+                            <div className='col-4'>
+                                <img src={require('./img/pokemon.jpg')} alt='pokemon' className='card-img' />
                             </div>
+                            <div className="col-8">
+                                <div className="card-body">
+                                    <div className='card-title h3 text-center'>
+                                        <CantidadPokemon />
+                                    </div>
+                                    <ComprarPokemon />
+                                </div>
 
-
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </Provider>
     );
 }
 
