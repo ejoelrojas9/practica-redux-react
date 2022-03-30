5774c39407e00c7fd86816c40ed513200ba54177
Joel Rojas <ejoelrojas9@gmail.com>
2022-03-30 11:24:35 -0500

    Configuración del buscador como muestra en pantalla en el archivo ResultadoPokemon.jsx

--- src/comṕonents/buscador/ResultadoPokemon.jsx
+++ src/comṕonents/buscador/ResultadoPokemon.jsx
@@ -1,16 +1,22 @@
+import { useSelector } from "react-redux";
 
 
 const ResultadoPokemon = () => {
+const buscador= useSelector((state) => state.buscador); //Llamar al estado actual, dentro de los parentesis la función anonima que esta pasando el state para devolver el state.buscador
+
   return(
   <div> 
     {/* div>h3.text-white{Resultado: }+div.text-warning{Buscador... }+.text-success>img+span{pikachu} */}
     <h3 className="text-white">Resultado: </h3>
-    <div className="text-warning">Buscando... </div>
-    <div className="text-success">
-      <img src="" alt="" />
-      <span>pikachu</span>
-      </div>
-      <span className="text-danger">Error</span>
+    {buscador.loading && <div className="text-warning">Buscando... </div>}
+    {
+        buscador.pokemon.length >= 1 && 
+        <div className="text-success">
+            <img src={buscador.pokemon[0].sprites.front_default} alt="Pokemon" />
+            <span>{buscador.pokemon[0].name}</span>
+        </div>
+    }
+      {buscador.error !== '' && <span className="text-danger">{buscador.error}</span>}
   </div>
   )
 }
