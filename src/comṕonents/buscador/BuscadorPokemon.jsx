139fbcfc43bcce07efaf118a1b5535cdaae0c182
Joel Rojas <ejoelrojas9@gmail.com>
2022-03-30 10:45:23 -0500

    Configuración de thunk para gestionar acciones azincronicas

--- src/comṕonents/buscador/BuscadorPokemon.jsx
+++ src/comṕonents/buscador/BuscadorPokemon.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import fetchPokemon from "../../redux/actions/buscadorAction";
+
+
 const BuscadorPokemon = () => { //Componente de función
+    const dispatch = useDispatch();   //Invocación del dispatch
+    const [pokemon_name, set_pokemon_name] = useState('mewtwo'); //Estado para los nombres del pokemon
+
   return(  //Dentro de la función regresamos algo que va ser un div con una clase que como hio va tener una label
     <div className="form-group">  {/* div.form-group>label.text-white+input.form-control#buscar_pokemon+button.btn.btn-primary.mt-3 */}
       <label htmlFor="buscar_pokemon" className="text-white">Buscar Pokemons</label>
-      <input type="text" className="form-control" id="buscar_pokemon" value="Pikachu" />
-      <button className="btn btn-primary mt-3">Enviar</button>
+      <input type="text" className="form-control" id="buscar_pokemon" 
+      value={pokemon_name} 
+      onChange={
+        (event) => {
+          set_pokemon_name(event.target.value);  {/*Con esto se esta cambiando el nombre del pokemon dinamicamente */}
+        }
+      }
+      />  {/* Se cambia el valor por el nombre */}
+      
+      <button className="btn btn-primary mt-3" onClick={
+        ()=> {
+          dispatch(fetchPokemon(pokemon_name))
+        }
+      }>Enviar</button>
     </div>
   )
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducers from "./reducers/rootReducers";
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension' //cuando esta en producción esto se quita
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducers, composeWithDevTools());
+const store = createStore(rootReducers, composeWithDevTools(
+    applyMiddleware(thunk)
+));
+
+// const store = createStore(rootReducers, applyMiddleware(thunk)); //en el caso de no tener composeWithDevTools
 
 export default store;
